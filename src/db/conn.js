549115605079f2ee7f12a49f9466b2d60c87aab8
AaqibbHasanie <24100233@lums.edu.pk>
2023-04-20 17:57:39 +0500

    css compatible for larger screeens
    haha

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,13 @@
-const mongoose = require("mongoose")
-// mongoose.connect("mongodb://127.0.0.1:27017/studybuddy",{ //used 127.0.0.1 instead of localhost
-mongoose.connect(process.env.MONGODB_URI,{
-}).then (()=> {
-    console.log(`connection successful`)
-}).catch((e) => {
-    console.log(`no connection`)
-    console.log(e)
-})
+const mongoose = require("mongoose");
+mongoose;
+//   .connect("mongodb://127.0.0.1:27017/studybuddy", {
+//used 127.0.0.1 instead of localhost
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => {
+    console.log(`connection successful`);
+  })
+  .catch((e) => {
+    console.log(`no connection`);
+    console.log(e);
+  });
