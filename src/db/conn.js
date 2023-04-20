a2838538191447352c85721790c3de89df5bc67b
AaqibbHasanie <24100233@lums.edu.pk>
2023-04-20 20:09:16 +0500

    all terminals made compatible for larger sccreens
    hah

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-mongoose;
-//   .connect("mongodb://127.0.0.1:27017/studybuddy", {
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/studybuddy", {})
 //used 127.0.0.1 instead of localhost
 mongoose
   .connect(process.env.MONGODB_URI, {})
