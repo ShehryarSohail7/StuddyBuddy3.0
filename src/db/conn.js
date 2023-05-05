0f2302c40192845559603a5303e4ad0db2b16bc2
Shehryar Sohail <101158096+ShehryarSohail7@users.noreply.github.com>
2023-05-06 00:00:08 +0500

    add login photo button removed

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // mongoose
-//   .connect("mongodb://127.0.0.1:27017/studybuddy", {})
+//   .connect("mongodb://127.0.0.1:27017/studybuddy", {}) //remember to switch between these.
 //used 127.0.0.1 instead of localhost
 mongoose
   .connect(process.env.MONGODB_URI, {})
